c0c93653362533e2d0a0dd5795ac109bd9542480
Siddhartha-Goutaman <siddharthgoutaman@gmail.com>
2024-05-04 22:21:23 +0530

    masked keys

--- Drone Based Monitoring/Code/alert1.py
+++ Drone Based Monitoring/Code/alert1.py
@@ -8,9 +8,9 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-password = "ymly ewzc trem qxgn"
-from_email = "siddharthgoutaman@gmail.com" 
-to_email = "siddharthgoutaman@icloud.com"
+password = "password"
+from_email = "from@gmail.com" 
+to_email = "to@gmail.com"
 server = smtplib.SMTP('smtp.gmail.com: 587')
 server.starttls()
 server.login(from_email, password)
